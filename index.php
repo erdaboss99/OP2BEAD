c8b0bdba95ae110a5e247b4f4ca881c2faf5e5c8
Erdélyi Roland <erdelyi.roland99@icloud.com>
2021-01-26 13:06:48 +0100

    Project Commit

---
+++ index.php
@@ -0,0 +1,59 @@
+<?php session_start();?>
+<!DOCTYPE html>
+<html>
+        <head>
+            <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+            <title>Hangman</title>
+        </head>
+    <body>
+        <?php
+            include 'config.php';
+            include 'functions.php';
+            include 'hangedman.php';
+            if (isset($_POST['newWord'])) unset($_SESSION['answer']);
+            if (!isset($_SESSION['answer']))
+            {
+                $_SESSION['attempts'] = 0;
+                $answer = fetchWordArray($WORDLISTFILE);
+                $_SESSION['answer'] = $answer;
+                $_SESSION['hidden'] = hideCharacters($answer);
+                echo 'Attempts remaining: '.($MAX_ATTEMPTS - $_SESSION['attempts']).'<br>';
+                echo '<br/> <pre>'.$hang[($MAX_ATTEMPTS - $_SESSION['attempts'])].'</pre> <br/>';
+            }else
+            {
+                if (isset ($_POST['userInput']))
+                {
+                    $userInput = $_POST['userInput'];
+                    $_SESSION['hidden'] = checkAndReplace(strtolower($userInput), $_SESSION['hidden'], $_SESSION['answer']);
+                    checkGameOver($MAX_ATTEMPTS,$_SESSION['attempts'], $_SESSION['answer'],$_SESSION['hidden']);
+                }
+                $_SESSION['attempts'] = $_SESSION['attempts'] + 1;
+                echo 'Attempts remaining: '.($MAX_ATTEMPTS - $_SESSION['attempts'])."<br>";
+                echo '<br/> <pre>'.$hang[($MAX_ATTEMPTS - $_SESSION['attempts'])].'</pre> <br/>';
+            }
+            $hidden = $_SESSION['hidden'];
+            foreach ((array)$hidden as $char) echo $char."  ";
+        ?>
+        <script type="application/javascript">
+            function validateInput()
+            {
+                var x=document.forms["inputForm"]["userInput"].value;
+                if (x=="" || x==" ")
+                {
+                    alert("Please enter a character.");
+                    return false;
+                }
+                if (!isNaN(x))
+                {
+                    alert("Please enter a character.");
+                    return false;
+                }
+            }
+        </script>
+        <form name = "inputForm" action = "" method = "post">
+            Your Guess: <input name = "userInput" type = "text" size="1" maxlength="1"  />
+            <input type = "submit"  value = "Check" onclick="return validateInput()"/>
+            <input type = "submit" name = "newWord" value = "Try another Word"/>
+        </form>
+    </body>
+</html>
